784c29cf4eb868226b8e0f8e0e415fa2f008b1d2
Kathleen B <kathleenb@Kathleens-MacBook-Air.local>
2020-10-08 15:04:23 -0400

    added functionality

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
-let run = () => {
-  let html = document.getElementById('html')
+window.onload = () => {
+  let iframe = document.querySelector('iframe');
+  let textarea = document.querySelectorAll('textarea')
+
+  for (let i = textarea.length - 1; i >= 0; i--){
+    if (textarea[i].addEventListener) {
+      textarea[i].addEventListener('input', function () {
+        iframe.srcdoc = textarea[0].value + '<style>' + textarea[1].value + '</style>' + '<script>' + textarea[2].value + '<\/script>'
+      }, false)
+    } else if (textarea[i].attachEvent) {
+      textarea[i].attachEvent('onpropertychange', function () {
+        iframe.srcdoc = textarea[0].value + '<style>' + textarea[1].value + '</style>' + '<script>' + textarea[2].value + '<\/script>'
+      })
+    }
+  }
 }
